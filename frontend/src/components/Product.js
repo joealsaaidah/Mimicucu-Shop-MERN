3e3b9538be923763784f74a2a2b0d48225f12a23
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-01 07:36:21 +0800

    fix product name

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -11,7 +11,7 @@ const Product = ({ product }) => {
       <Card.Body>
         <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
-            <strong>product.name</strong>
+            <strong>{product.name}</strong>
           </Card.Title>
         </Link>
         <Card.Text as='div'>
