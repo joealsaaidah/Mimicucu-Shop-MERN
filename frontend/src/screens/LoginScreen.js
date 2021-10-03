e0bf9498c116f469b57d710d31976615322690ac
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-04 06:35:56 +0800

    frontend auth - show user in th navbar& logout

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -15,7 +15,7 @@ const LoginScreen = ({ history, location }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { loading, error, userInfo } = userLogin;
   const redirect = location.search ? location.search.split("=")[1] : "/";
-
+  console.log(redirect);
   useEffect(() => {
     if (userInfo) {
       history.push(redirect);
