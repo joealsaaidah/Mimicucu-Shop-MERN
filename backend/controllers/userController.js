4d96ba0b3a080a72ce08aef33d41eee4b4779a3b
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-06 04:08:56 +0800

    admin - middleware & endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,4 +90,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/ admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
