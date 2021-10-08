a933f2bdb5a0c456d2d2764a3f2e678e3e329f48
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-08 23:27:27 +0800

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("api is running...");
-});
-
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -33,8 +29,21 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
 const __dirname = path.resolve(); // if using es6
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
