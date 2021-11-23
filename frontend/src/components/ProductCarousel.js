94c80e6716541f8c705af6cb90605fa04175a016
joealsaaidah <alsaaidahyousef@gmail.com>
2021-11-23 14:49:30 +0800

    fix the carousel img positioning

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-import { Carousel, Image } from "react-bootstrap";
+import { Carousel, Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { listTopRatedProduct } from "../actions/productActions";
 import Loader from "./Loader";
 import Message from "./Message";
 import { LinkContainer } from "react-router-bootstrap";
+
 const ProductCarousel = () => {
   const dispatch = useDispatch();
 
@@ -25,10 +26,16 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <img
+              class='d-block mx-auto'
+              src={product.image}
+              alt={product.name}
+              fluid
+            />
+
             <Carousel.Caption className='carousel-caption'>
               <h2>
-                {product.name} (${product.price})
+                {product.name} ($ {product.price})
               </h2>
             </Carousel.Caption>
           </Link>
